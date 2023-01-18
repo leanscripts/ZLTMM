a92b1fa660cb4b8f60eb84cf5f9d3c0608d864cf
leanscripts <peterofielu14@gmail.com>
2023-01-18 10:36:40 +0100

    initial commit

--- app.js
+++ app.js
@@ -37,3 +37,37 @@ document.querySelector('#push').onclick = function () {
     document.querySelector('#taskInput').value = '';
 }
 
+      // Get the animated square element
+      const square = document.getElementById('animated-square');
+
+      // Set the initial position of the square
+      square.style.left = '0px';
+      
+      // Set the animation speed (in milliseconds)
+      const animationSpeed = 10;
+
+      // Start the animation loop
+      setInterval(() => {
+        // Get the current position of the square
+        const currentPosition = parseInt(square.style.left);
+
+        // Update the position of the square
+        square.style.left = `${currentPosition + 1}px`;
+      }, animationSpeed);
+
+
+      function start(al) {
+        var bar = document.getElementById('progressBar');
+        var status = document.getElementById('status');
+        status.innerHTML = al + "%";
+        bar.value = al;
+        al++;
+        var sim = setTimeout("start(" + al + ")", 15000);
+        if (al == 100) {
+          status.innerHTML = "100%";
+          bar.value = 100;
+          clearTimeout(sim);
+          var finalMessage = document.getElementById('finalMessage');
+          finalMessage.innerHTML = "Process is complete";
+        }
+      }
